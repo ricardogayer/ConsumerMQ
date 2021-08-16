3cfedcf0c2a3b12d477fb3ea923589087f1c023a
Ricardo R. Gayer <ricardo.r.gayer@gmail.com>
2021-08-15 23:44:45 -0300

    versao inicial

---
+++ go.mod
@@ -0,0 +1,5 @@
+module ConsumerMQ
+
+go 1.16
+
+require github.com/streadway/amqp v1.0.0
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/streadway/amqp"
+	"log"
+)
+
+type Request struct {
+	URL string `json:"url"`
+}
+
+func main() {
+
+	amqpConnection, err := amqp.Dial("amqp://guest:password@localhost:5672/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer amqpConnection.Close()
+
+	channelAmqp, _ := amqpConnection.Channel()
+	defer channelAmqp.Close()
+
+	forever := make(chan bool)
+
+	msgs, err := channelAmqp.Consume(
+		"rss_urls",
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+
+			var request Request
+			json.Unmarshal(d.Body, &request)
+
+			log.Println("RSS URL:", request.URL)
+
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
